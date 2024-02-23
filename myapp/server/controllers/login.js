b37f94378940f8c9cb5bc5065bc89ec4769d8e0a
LeeYongwuk <140808035+dnslfkrh@users.noreply.github.com>
2024-02-24 04:40:49 +0900

    Update login.js

--- myapp/server/controllers/login.js
+++ myapp/server/controllers/login.js
@@ -10,9 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.post('/logout', (req, res) => {
-    // res.clearCookie('user_id');
     res.json({ message: '로그아웃 성공' });
-    console.log('로그아웃합니다.');
 });
 
 app.post('/login', async (req, res) => {
@@ -31,16 +29,12 @@ app.post('/login', async (req, res) => {
             return res.status(200).json({ message: '로그인 실패' });
         }
 
-        console.log('로그인 성공:', log_id);
-        // 쿠키
         res.cookie('user_id', log_id, { maxAge: 900000, httpOnly: true, sameSite: 'None', secure: true });
-        console.log('쿠키 생성됨');
         res.status(200).cookie('user_id', log_id).json({ message: '로그인 성공' });
 
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: '서버 오류' });
-        console.log('서버 오류');
     }
 });
 
